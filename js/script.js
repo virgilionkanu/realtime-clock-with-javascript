e834cffe4f71da576cf4d9394adcb62679ab17e1
Virgílio Cano <virgiliocano53@gmail.com>
2020-03-30 22:26:16 -0300

    first commit... AND DONE!

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function realtimeClock() {
+    var rtClock = new Date();
+
+    var hours = rtClock.getHours();
+    var minutes = rtClock.getMinutes();
+    var seconds = rtClock.getSeconds();
+
+    //AM and PM system
+    var amPm = (hours < 12) ? "AM" : "PM";
+
+    //Convert the hours to 12-hour format
+    hours = (hours > 12) ? - 12 : hours;
+
+    //
+    hours = ("0" + hours).slice(-2);
+    minutes = ("0" + minutes).slice(-2);
+    seconds = ("0" + seconds).slice(-2);
+
+    //Display the clock
+    document.getElementById('clock').innerHTML = 
+        hours + " : " + minutes + " : " + seconds + " " + amPm;
+    var t = setTimeout(realtimeClock, 500);
+}
